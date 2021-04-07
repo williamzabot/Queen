90afd7dcb8bd899972eade3e77412765da051c4b
williamcansizabot <william.zabot@eldorado.org.br>
2021-04-07 18:48:44 -0300

    updated hilt version

--- app/src/main/java/com/williamzabot/queens/data/di/RetrofitModule.kt
+++ app/src/main/java/com/williamzabot/queens/data/di/RetrofitModule.kt
@@ -4,7 +4,7 @@ import com.williamzabot.queens.data.api.QueenApi
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
-import dagger.hilt.android.components.ApplicationComponent
+import dagger.hilt.components.SingletonComponent
 import okhttp3.OkHttpClient
 import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
@@ -12,7 +12,7 @@ import retrofit2.converter.gson.GsonConverterFactory
 import javax.inject.Singleton
 
 @Module
-@InstallIn(ApplicationComponent::class)
+@InstallIn(SingletonComponent::class)
 object RetrofitModule {
 
     @Singleton
